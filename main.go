fc49135737a5e31ea763634fab5b08960b0ccc15
Vlados <vidgimka4@gmail.com>
2024-07-26 23:56:20 +0300

    вызов конкретного элемента объекта и работа с ним

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import "fmt"
 
+var geodatList []GeoData
+
 func main() {
 	fmt.Println("Парсинг json")
-	fmt.Println(readFileData())
-	readFileData()
+	//fmt.Println(readFileData()) просто выводился джейсон в терминал
+	geodatList = readFileData()
 
+	//fmt.Println(geodatList[2]) выводился конкернтый объект джейсона
+	fmt.Println(geodatList[2].Station_distance + 2) // один из элементов для последующих действий
 }
